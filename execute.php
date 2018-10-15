eca7c16be33a768f2f147fb9f07fd0d92002bb2f
cacosdev <44161558+cacosdev@users.noreply.github.com>
2018-10-15 14:08:44 +0200

    done

---
+++ execute.php
@@ -0,0 +1,40 @@
+<?php
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+if(!$update)
+{
+  exit;
+}
+
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+
+$text = trim($text);
+$text = strtolower($text);
+
+$stickers = array("CAADBAADAQcAApEMbAtICGAYZ93cWAI", "CAADAgADHAADyIsGAAFzjQavel2uswI", "CAADBAADKwADfXowBxSsyRbvJAthAg");
+
+header("Content-Type: application/json");
+switch($text)
+	{	
+		case "/sticker":
+			$parameters = array('chat_id' => $chatId, "sticker" => $stickers[rand(0,count($stickers) - 1)]);
+			$parameters["method"] = "sendSticker";
+		break;
+		
+		case "/infomsg":
+			$parameters = array('chat_id' => $chatId, "text" => $update);
+			$parameters["method"] = "sendMessage";
+		break;
+		
+	}
+	
+	echo json_encode($parameters);
+	
